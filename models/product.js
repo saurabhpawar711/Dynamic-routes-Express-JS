9b45ecead9a1f7ddf80b03ca1eaabcaa483b1e59
saurabhpawar711 <saurabhpawar1107@gmail.com>
2023-06-25 11:25:52 +0530

    Dynamic Routes Part 3 (Query Params)

--- models/product.js
+++ models/product.js
@@ -1,8 +1,10 @@
+const { fileLoader } = require('ejs');
 const fs = require('fs');
 const path = require('path');
+const Cart = require('./cart');
 
 const p = path.join(
-  __dirname,'../',
+  __dirname, '../',
   'data',
   'products.json'
 );
@@ -18,7 +20,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +29,23 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
+
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id)
+        const updatedProduct = [...products];
+        updatedProduct[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+          console.log(err);
+        });
+      }
+      else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
@@ -39,9 +53,21 @@ module.exports = class Product {
     getProductsFromFile(cb);
   }
 
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id)
+      const updatedProduct = products.filter(prod => prod.id !== id)
+      fs.writeFile(p, JSON.stringify(updatedProduct), (err) => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price)
+        }
+      })
+    })
+  }
+
   static getById(id, cb) {
     getProductsFromFile(products => {
-      const product = products.find(p => p.id===id)
+      const product = products.find(p => p.id === id)
       cb(product);
     })
   }
